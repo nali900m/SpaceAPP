5940337e0f5c777b0445dab47402b1862570a8de
Nassim <n.ali900m@gmail.com>
2024-04-12 14:53:49 +0200

    rockets

--- SpaceApp/CustomComponents/RocketsLists.js
+++ SpaceApp/CustomComponents/RocketsLists.js
@@ -1,16 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList, Image, ScrollView } from 'react-native';
 
 const PostRockets = () => {
-    const [posts, setPosts] = useState([]);
+    const [rockets, setRockets] = useState([]);
     const [loading, setLoading] = useState(false);
 
     const fetchPosts = async () =>{ 
         fetch('https://api.spacexdata.com/v4/rockets')
         .then(response=>response.json())
         .then(data=>{
-            data = data[1]
-            setPosts(data.name)
+            setRockets(data)
         })
         .catch(e=>{
             console.log(e)
@@ -26,13 +25,14 @@ const PostRockets = () => {
             {loading ? (
                 <ActivityIndicator />
             ) : (
-                <FlatList 
-                data={posts}
-                keyExtractor={({id}) => id}
-                renderItem={({ item}) => (
-                    <Text>{`${posts}\n`}</Text>
-                )}
-                />
+                rockets.map((rocket) => {
+                    return(
+                        <ScrollView>
+                            <Text>{rocket.name}</Text>
+                        </ScrollView>
+                    )
+                    
+                })
             )}
         </View>
     );
