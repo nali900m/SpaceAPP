9bb9bfb7f4ee0caa9a429171289b4f0b70ed05fd
Nassim <n.ali900m@gmail.com>
2024-04-12 10:12:51 +0200

    Création pages

--- SpaceApp/App.js
+++ SpaceApp/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Compagnies from './CustomPages/Compagnies';
+import Accueil from './CustomPages/Accueil';
+import Fusées from './CustomPages/Fusées';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Accueil" component={Accueil} />
+        <Tab.Screen name="Compagnies" component={Compagnies} />
+        <Tab.Screen name="Fusées" component={Fusées} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    </>
   );
 }
 
